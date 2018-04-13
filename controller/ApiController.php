653cf0367a900a0aa35975b01586605ccc8df2dd
HP <hpkjaer@protonmail.com>
2018-04-13 20:51:42 +0200

    Added time and get all wo action

--- controller/ApiController.php
+++ controller/ApiController.php
@@ -1,24 +1,6 @@
 <?php 
 class ApiController extends BaseController {
 
-	// /mt/api/workout?id=4
-	/*public function workoutAction($id){
-
-		$mapper = $this->modelFactory->buildMapper('WorkoutModelMapper');
-		$model = $this->modelFactory->buildObject('WorkoutModel');
-
-		$status = $mapper->fetchById($model, $id);
-
-		
-		if($status){
-			$this->view->outputJSON(get_object_vars($model));
-		}
-		else{
-			// something meaningful
-		}
-	}
-*/
-	// /mt/api/exercise?id=4
 	public function exerciseAction($id){
 
 		$mapper = $this->modelFactory->buildMapper('ExerciseModelMapper');
@@ -33,7 +15,6 @@ class ApiController extends BaseController {
 	}
 
 	public function workoutAction($id){
-
 		$mapper = $this->modelFactory->buildMapper('WorkoutModelMapper');
 		$model = $this->modelFactory->buildObject('WorkoutModel');
 
@@ -45,21 +26,36 @@ class ApiController extends BaseController {
 		}
 		else{
 			$this->view->outputJSON(['success' => false, 'message' => 'No workout found']);
-			// something meaningful
 		}
 	}
+
 	public function searchWorkoutAction($diff = null, $focus = null, $time = null, $c_wid = null){
 		$workoutListModel = $this->modelFactory->buildObject('WorkoutListModel');
 		$workoutModelMapper = $this->modelFactory->buildMapper('WorkoutModelMapper');
 		$workout;
 
-    	if($diff != null){
+    	if($diff != null)
     		$diff = round($diff);
-    		$workoutModelMapper->search($workoutListModel, 1, 1, null, null, 'rand', $diff, $focus, $c_wid);
-    		$workout =  (count($workoutListModel->workouts) > 0) ? $workoutListModel->workouts[0] : null;
-    	}
+    	
+    	$workoutModelMapper->search($workoutListModel, 1, 1, null, null, 'rand', $diff, $focus, $time, $c_wid);
+    	$workout =  (count($workoutListModel->workouts) > 0) ? $workoutListModel->workouts[0] : null;
+
     	if($workout){
 			$this->view->outputJSON(['success' => true, 'message' => '', 'data' => get_object_vars($workout)]);
+    	}
+    	else{
+    		$this->view->outputJSON(['success' => false, 'message' => '']);
+    	}
+	}
+
+	public function getAllWorkoutsAction(){
+		$workoutListModel = $this->modelFactory->buildObject('WorkoutListModel');
+		$workoutModelMapper = $this->modelFactory->buildMapper('WorkoutModelMapper');
+
+    	$workoutModelMapper->search($workoutListModel, 99999, 1, null, null, 'name-asc');
+    	
+    	if($workout){
+			$this->view->outputJSON(['success' => true, 'message' => '', 'data' => get_object_vars($workoutListModel)]);
     	}
     	else{
     		$this->view->outputJSON(['success' => false, 'message' => '']);
